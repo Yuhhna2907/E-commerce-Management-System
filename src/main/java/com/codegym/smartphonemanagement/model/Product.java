ad87a76994f4b46b3eca4e1d9c9d7c868fcf4093
Lương Anh Huy <luonganhhuy2004@gmail.com>
2026-04-03 10:55:03 +0700

    commit

--- src/main/java/com/codegym/smartphonemanagement/model/Product.java
+++ src/main/java/com/codegym/smartphonemanagement/model/Product.java
@@ -55,5 +55,11 @@ public class Product {
     @PrePersist
     public void prePersist() {
         this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
     }
 }
