26d44ddb9ef6a063194193bf3f1395af16db006a
Lương Anh Huy <luonganhhuy2004@gmail.com>
2026-04-06 08:52:43 +0700

    commit

--- src/main/java/com/codegym/smartphonemanagement/service/product/seller/ProductService.java
+++ src/main/java/com/codegym/smartphonemanagement/service/product/seller/ProductService.java
@@ -1,4 +1,4 @@
-package com.codegym.smartphonemanagement.service.product.admin;
+package com.codegym.smartphonemanagement.service.product.seller;
 
 import com.codegym.smartphonemanagement.model.Product;
 import com.codegym.smartphonemanagement.repository.ProductRepository;
